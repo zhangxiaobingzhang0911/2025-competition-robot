7ea5b218fc740e5cb1095469c39cf5ffc53d4ea6
RealAllenDa <2153867950@qq.com>
2025-02-12 22:31:17 +0800

    feat: add elevator simulation

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
@@ -1,30 +1,25 @@
 package frc.robot.subsystems.elevator;
 
-import com.ctre.phoenix6.controls.VoltageOut;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.units.AngularVelocityUnit;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.VoltageUnit;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
-import frc.robot.RobotConstants;
 
 import static edu.wpi.first.units.Units.*;
+import static frc.robot.RobotConstants.ElevatorConstants.ELEVATOR_GEAR_RATIO;
+import static frc.robot.RobotConstants.ElevatorConstants.ELEVATOR_SPOOL_DIAMETER;
 
 public class ElevatorIOSim implements ElevatorIO {
     private static final double LOOP_PERIOD_SECS = 0.02;
     private final DCMotorSim leftElevatorTalonSim = new DCMotorSim(edu.wpi.first.math.system.plant.LinearSystemId.createDCMotorSystem(DCMotor.getFalcon500(1),
             0.025, 6.75), DCMotor.getFalcon500(1), null);
-    private final DCMotorSim rightElevatorTalonSim = new DCMotorSim(edu.wpi.first.math.system.plant.LinearSystemId.createDCMotorSystem(DCMotor.getFalcon500(1),
-            0.025, 6.75), DCMotor.getFalcon500(1) , null);
 
     private Measure<VoltageUnit> leftElevatorAppliedVoltage = Volts.zero();
-    private Measure<VoltageUnit> rightElevatorAppliedVoltage = Volts.zero();
-    private Measure<AngularVelocityUnit> targetElevatorVelocity = RadiansPerSecond.zero();
 
     @Override
     public void updateInputs(ElevatorIOInputs inputs) {
         leftElevatorTalonSim.update(LOOP_PERIOD_SECS);
-        rightElevatorTalonSim.update(LOOP_PERIOD_SECS);
 
         inputs.leftElevatorVelocity =
                 RadiansPerSecond.of(leftElevatorTalonSim.getAngularVelocityRadPerSec());
@@ -36,30 +31,35 @@ public class ElevatorIOSim implements ElevatorIO {
                 Amps.of(leftElevatorTalonSim.getCurrentDrawAmps());
 
         inputs.rightElevatorVelocity =
-                RadiansPerSecond.of(rightElevatorTalonSim.getAngularVelocityRadPerSec());
+                RadiansPerSecond.of(leftElevatorTalonSim.getAngularVelocityRadPerSec());
         inputs.rightElevatorPosition =
-                Radians.of(rightElevatorTalonSim.getAngularPositionRad());
+                Radians.of(leftElevatorTalonSim.getAngularPositionRad());
         inputs.rightElevatorAppliedVoltage =
-                rightElevatorAppliedVoltage;
+                leftElevatorAppliedVoltage;
         inputs.rightElevatorSupplyCurrent =
-                Amps.of(rightElevatorTalonSim.getCurrentDrawAmps());
+                Amps.of(leftElevatorTalonSim.getCurrentDrawAmps());
     }
 
     @Override
     public void setElevatorDirectVoltage(double volts) {
+        leftElevatorAppliedVoltage = Volts.of(volts);
+        leftElevatorTalonSim.setInputVoltage(volts);
     }
 
     @Override
-    public void resetElevatorPosition(){
+    public void resetElevatorPosition() {
+        leftElevatorTalonSim.setAngle(0);
     }
 
     @Override
     public boolean isNearExtension(double expected) {
-        return true;
+        return MathUtil.isNear(metersToRotations(expected), leftElevatorTalonSim.getAngularPositionRad(), 0);
     }
 
     @Override
     public void setElevatorTarget(double radians) {
+        // FIXME: Adding delays?
+        leftElevatorTalonSim.setAngle(radians);
     }
 
     @Override
@@ -69,10 +69,19 @@ public class ElevatorIOSim implements ElevatorIO {
 
     @Override
     public boolean isNearZeroExtension() {
-        return true;
+        return MathUtil.isNear(metersToRotations(0.05), leftElevatorTalonSim.getAngularPositionRad(), 0.3);
     }
+
     @Override
     public double getElevatorVelocity() {
-        return rightElevatorTalonSim.getAngularVelocityRadPerSec() / 6.28 * 60;
+        return leftElevatorTalonSim.getAngularVelocityRadPerSec() / 6.28 * 60;
+    }
+
+    private double metersToRotations(double heightMeters) {
+        return (heightMeters / (Math.PI * ELEVATOR_SPOOL_DIAMETER)) * ELEVATOR_GEAR_RATIO;
+    }
+
+    private double rotationsToMeters(double rotations) {
+        return rotations * (Math.PI * ELEVATOR_SPOOL_DIAMETER) / ELEVATOR_GEAR_RATIO;
     }
 }

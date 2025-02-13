006a33d9da2d50ed1288bac38ca8a340f006be41
LCsiu <148622803+LouisCaiKM7@users.noreply.github.com>
2025-02-13 11:18:30 +0800

    Revert "refactor: elevator clean up"
    This reverts commit b84d0a30a45e32567305e05d8c43354112bddd9d.

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
@@ -7,6 +7,22 @@ import org.littletonrobotics.junction.AutoLog;
 import static edu.wpi.first.units.Units.*;
 
 public interface ElevatorIO {
+    @AutoLog
+    class ElevatorIOInputs {
+        public double positionMeters = 0.0;
+        public double velocityMetersPerSec = 0.0;
+        public double motionMagicVelocityTarget = 0.0;
+        public double motionMagicPositionTarget = 0.0;
+        public double setpointMeters = 0.0;
+        public double[] appliedPosition = new double[] {}; // {leader, follower}
+        public double[] appliedVelocity = new double[] {}; // {leader, follower}
+        public double[] appliedVolts = new double[] {}; // {leader, follower}
+        public double[] statorCurrentAmps = new double[] {}; // {leader, follower}
+        public double[] supplyCurrentAmps = new double[] {}; // {leader, follower}
+        public double[] tempCelsius = new double[] {}; // {leader, follower}
+        // public double acceleration = 0.0; may add later
+    }
+
     void updateInputs(ElevatorIOInputs inputs);
 
     void setElevatorDirectVoltage(double volts);
@@ -24,21 +40,4 @@ public interface ElevatorIO {
     boolean isNearExtension(double expected);
 
     boolean isNearZeroExtension();
-
-    @AutoLog
-    class ElevatorIOInputs {
-        public double positionMeters = 0.0;
-        public double velocityMetersPerSec = 0.0;
-        public double motionMagicVelocityTarget = 0.0;
-        public double motionMagicPositionTarget = 0.0;
-        public double setpointMeters = 0.0;
-        public double[] appliedPosition = new double[] {}; // {leader, follower}
-        public double[] appliedVelocity = new double[] {}; // {leader, follower}
-        public double[] appliedVolts = new double[] {}; // {leader, follower}
-        public double[] statorCurrentAmps = new double[] {}; // {leader, follower}
-        public double[] supplyCurrentAmps = new double[] {}; // {leader, follower}
-        public double[] tempCelsius = new double[] {}; // {leader, follower}
-        // public double acceleration = 0.0; may add later
-    }
-
 }

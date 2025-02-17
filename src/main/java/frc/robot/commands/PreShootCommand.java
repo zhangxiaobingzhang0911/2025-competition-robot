b85851a614743d701790968c2e02cba1e542dde4
Eason Zhang <eeeaaason070311@gmail.com>
2025-02-17 21:05:30 +0800

    Feat: put coral command

--- src/main/java/frc/robot/commands/PreShootCommand.java
+++ src/main/java/frc/robot/commands/PreShootCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
 import frc.robot.subsystems.endeffector.EndEffectorSubsystem;
 import frc.robot.subsystems.intake.IntakeSubsystem;
@@ -11,20 +12,22 @@ public class PreShootCommand extends Command {
     private final EndEffectorSubsystem endEffectorSubsystem;
     private final IntakeSubsystem intakeSubsystem;
     private final ElevatorSubsystem elevatorSubsystem;
-    private final double elevatorSetPoint;
 
-    public PreShootCommand(EndEffectorSubsystem endEffectorSubsystem, IntakeSubsystem intakeSubsystem, ElevatorSubsystem elevatorSubsystem, double elevatorSetPoint) {
+
+    public PreShootCommand(EndEffectorSubsystem endEffectorSubsystem, IntakeSubsystem intakeSubsystem, ElevatorSubsystem elevatorSubsystem) {
         this.endEffectorSubsystem = endEffectorSubsystem;
         this.intakeSubsystem = intakeSubsystem;
         this.elevatorSubsystem = elevatorSubsystem;
-        this.elevatorSetPoint = elevatorSetPoint;
+
     }
 
     @Override
         public void execute() {
             intakeSubsystem.setWantedState(IntakeSubsystem.WantedState.HOME);
-            elevatorSubsystem.setElevatorPosition(elevatorSetPoint);
+            elevatorSubsystem.setElevatorPosition(RobotContainer.elevatorTargetPosition);
+            System.out.println(RobotContainer.elevatorTargetPosition);
             endEffectorSubsystem.setWantedState(EndEffectorSubsystem.WantedState.PRE_SHOOT);
+            //System.out.println(elevatorSetPoint);
         }
 
     @Override

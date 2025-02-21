c58daf6d139375c7eb2692fed0dca669ec9f388e
RealAllenDa <2153867950@qq.com>
2025-02-21 22:36:09 +0800

    feat: add auto
    Complete auto parsing and trajectory following.

---
+++ src/main/java/frc/robot/auto/basics/CustomAutoChooser.java
@@ -0,0 +1,36 @@
+package frc.robot.auto.basics;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+import java.util.List;
+
+import static com.pathplanner.lib.auto.AutoBuilder.getAllAutoNames;
+
+// adapted from AutoBuilder since we do not initialize AutoBuilder.
+public class CustomAutoChooser {
+    public static SendableChooser<String> buildAutoChooser() {
+        return buildAutoChooser("");
+    }
+
+    public static SendableChooser<String> buildAutoChooser(String defaultAutoName) {
+        SendableChooser<String> chooser = new SendableChooser<>();
+        List<String> autoNames = getAllAutoNames();
+        String defaultOption = null;
+
+        for (String autoName : autoNames) {
+            if (!defaultAutoName.isEmpty() && defaultAutoName.equals(autoName)) {
+                defaultOption = autoName;
+            }
+            chooser.addOption(autoName, autoName);
+        }
+
+        if (defaultOption == null) {
+            chooser.setDefaultOption("None", "");
+        } else {
+            chooser.setDefaultOption(defaultOption, defaultOption);
+            chooser.addOption("None", "");
+        }
+
+        return chooser;
+    }
+}

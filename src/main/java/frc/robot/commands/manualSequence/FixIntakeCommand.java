3aa2b83211121b08222eb9d538e15c40619e17ba
hs150521 <hs150521@163.com>
2025-04-12 14:39:29 +0800

    [Fix] FixIntakeCommand

--- src/main/java/frc/robot/commands/manualSequence/FixIntakeCommand.java
+++ src/main/java/frc/robot/commands/manualSequence/FixIntakeCommand.java
@@ -1,14 +1,13 @@
 package frc.robot.commands.manualSequence;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.RobotConstants;
-import frc.robot.subsystems.climber.ClimberSubsystem;
-import frc.robot.subsystems.climber.ClimberSubsystem.WantedState;
+import frc.robot.drivers.GamepieceTracker;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
 import frc.robot.subsystems.endeffectorarm.EndEffectorArmSubsystem;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 
 import static frc.robot.RobotConstants.ElevatorConstants.HOLD_EXTENSION_METERS;
+import static frc.robot.RobotConstants.ElevatorConstants.HOME_EXTENSION_METERS;
 
 public class FixIntakeCommand extends Command {
     private final ElevatorSubsystem elevatorSubsystem;
@@ -16,7 +15,7 @@ public class FixIntakeCommand extends Command {
     private final EndEffectorArmSubsystem endEffectorArmSubsystem;
 
     public FixIntakeCommand(ElevatorSubsystem elevatorSubsystem,
-                        IntakeSubsystem intakeSubsystem, EndEffectorArmSubsystem endEffectorArmSubsystem) {
+                            IntakeSubsystem intakeSubsystem, EndEffectorArmSubsystem endEffectorArmSubsystem) {
         this.elevatorSubsystem = elevatorSubsystem;
         this.endEffectorArmSubsystem = endEffectorArmSubsystem;
         this.intakeSubsystem = intakeSubsystem;
@@ -32,8 +31,18 @@ public class FixIntakeCommand extends Command {
 
     @Override
     public void end(boolean interrupted) {
-        elevatorSubsystem.setElevatorPosition(RobotConstants.ElevatorConstants.HOME_EXTENSION_METERS.get());
-        endEffectorArmSubsystem.setWantedState(EndEffectorArmSubsystem.WantedState.CORAL_INTAKE);
+        intakeSubsystem.setWantedState(IntakeSubsystem.WantedState.HOME);
+        if (!GamepieceTracker.getInstance().isEndeffectorHasCoral() && !GamepieceTracker.getInstance().isEndeffectorHasAlgae()) {
+            elevatorSubsystem.setElevatorPosition(HOME_EXTENSION_METERS.get());
+        } else {
+            elevatorSubsystem.setElevatorPosition(HOLD_EXTENSION_METERS.get());
+        }
+        endEffectorArmSubsystem.setWantedState(EndEffectorArmSubsystem.WantedState.HOLD);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return endEffectorArmSubsystem.hasCoral();
     }
 
     @Override
